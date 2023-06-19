590fcf4bdc2da3bcb3529d67375e4ae341eff8f6
Muhammad Amin <mhdamin.halim@gmail.com>
2023-06-19 16:11:10 +0800

    Update DistanceCalculatorRestServiceApplicationTests.java
    added tests

--- src/test/java/com/example/distancecalculatorrestservice/DistanceCalculatorRestServiceApplicationTests.java
+++ src/test/java/com/example/distancecalculatorrestservice/DistanceCalculatorRestServiceApplicationTests.java
@@ -1,7 +1,14 @@
 package com.example.distancecalculatorrestservice;
 
+import com.example.distancecalculatorrestservice.utils.DistanceUnitConversion;
+import com.example.distancecalculatorrestservice.utils.MathUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DistanceCalculatorRestServiceApplicationTests {
@@ -9,5 +16,23 @@ class DistanceCalculatorRestServiceApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void whenConvertingFromYardsToMeter_thenConversionMustBeValid(){
+		assertEquals(DistanceUnitConversion.convert("meter", 1, "yards"), 1.0936132983377);
+	}
+
+	@Test
+	void whenConvertingFromMeterToYard_thenConversionMustBeValid(){
+		assertEquals(DistanceUnitConversion.convert("yards", 1, "meter"), 0.9144);
+	}
+
+	@Test
+	void whenAddingSumOfAListOfDouble_thenTheSumMustBeValid() {
+		List<Double> list = Arrays.asList(1.38, 2.56, 4.3);
+		assertEquals(MathUtils.sum(list), 8.24);
+
+	}
+
 
 }
